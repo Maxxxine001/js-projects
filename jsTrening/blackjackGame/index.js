1bfc014cd63409f786b5126b79fa5e991355e105
Maxine 001 <264492@student.pwr.edu.pl>
2022-10-23 21:03:14 +0200

    finish Black Jack Game

--- jsTrening/blackjackGame/index.js
+++ jsTrening/blackjackGame/index.js
@@ -1,52 +1,55 @@
-let firstCard = getRandomcard()
-let secondCard = getRandomcard()
-let thirdCard = getRandomcard()
-let cards = [firstCard, secondCard]
-let sum = cards[0] + cards[1]
-let hasBlackJack = false
-let isAlive = true
+let cards = []
+let sum = 0
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.querySelector("#sum-el")
 let cardsEl = document.getElementById("cards-el")
-console.log(messageEl)
+const BLACK_JACK_VALUE = 21
 
-
-function renderGame() {
-    
-    startRound()
-    
+function getRandomcard(){
+    let randomNumber = Math.floor(Math.random() * 10 + 2)
+    return randomNumber
 }
 
-function startRound() {
-   
-    messageEl.textContent = message
-   sumEl.textContent = "Sum: " + sum 
-    for( let i = 0; i < cards.length ; i++)
-        {
-           cardsEl.textContent += cards[i] + " "
-        }
 
-    if(sum <=20 ) {
-        message = 'Do you want to draw a new card ?'
-    } else if (sum === 21) {
-        message = 'Black jack!'
-        hasBlackJack = true
-    } else {
-        message= "You've lost :("
-        isAlive = false
+function startRound() {
+    cards = [getRandomcard(), getRandomcard()]
+    sum = cards[0] + cards[1]
+    sumEl.textContent = `Sum: ${sum}`
+    cardsEl.textContent = ''
+
+    for(let i = 0; i < cards.length ; i++) {
+        cardsEl.textContent += cards[i] + " "
     }
+
+    checkGameState()
 }
 
 function newCard() {
-    
-    cards.push(thirdCard)
-    sum += cards[2]
-    cardsEl.textContent += " " + cards[2]
-    sumEl.textContent = "Sum: " + sum 
+    const card = getRandomcard()
+    cards.push(card)
+    sum += card
+    cardsEl.textContent += ` ${card}`
+    sumEl.textContent = `Sum: ${sum}`
+    messageEl.textContent = message
+
+    checkGameState()
 }
 
-function getRandomcard(){
-    let randomNumber = Math.floor(Math.random() * 9 + 2)
-    return randomNumber
+
+function checkGameState() {
+    if(sum < BLACK_JACK_VALUE) {
+        messageEl.textContent = 'Do you want to draw a new card ?'
+        return
+    }
+
+    if(sum === BLACK_JACK_VALUE) {
+        messageEl.textContent = 'Black jack!'
+        return 
+    }
+
+    if (sum > BLACK_JACK_VALUE){
+        messageEl.textContent = "You've lost :("
+        return
+    }
 }

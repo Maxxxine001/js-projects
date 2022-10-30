da74495e8a050df8a470707927c4c51ff006b084
Maxine 001 <264492@student.pwr.edu.pl>
2022-10-30 23:51:32 +0100

    complete bet function, complete score board, css changes

--- jsTrening/blackjackGame/index.js
+++ jsTrening/blackjackGame/index.js
@@ -5,13 +5,23 @@ let playerCash = 50
 let playerCashEl = document.getElementById("player-cash-amount")
 playerCashEl.textContent = `Player Cash : ${playerCash}`
 
-let didPlayerWon = false 
-let didPlayerSurrender = false
+
+const GameStates = {
+    win: 0,
+    lose: 1,
+    surrender: 2,
+    inGame: 3
+}
+
+let gameState = GameStates.inGame
+
+
 const BLACK_JACK_VALUE = 21
 const cardNames = {11: "jack", 12: "queen", 13: "king", 14: "ace"}
 const cardColors = {1: "clubs", 2: "diamonds" , 3: "hearts", 4: "spades"}
 const newCardButton = document.getElementById('new-card-button')
 const betButton = document.getElementById("bet-button")
+const betResultsButton = document.getElementById("betResults-button")
 let messageEl = document.getElementById("message-el")
 let sumEl = document.querySelector("#sum-el")
 let cardsEl = document.getElementById("cards-el")
@@ -29,11 +39,16 @@ function getRandomColor() {
 
 const startButton = document.getElementById('start-button')
 
+
+
 function startRound() {
     startButton.disabled = true
     standButton.disabled = false
     newCardButton.disabled = false
     surrenderButton.disabled = false
+    betButton.disabled = true
+    
+
 
     cardsEl.textContent = ''
     sumEl.textContent = ''
@@ -68,9 +83,15 @@ const standButton = document.getElementById('stand-button')
 function stand(){
     if(casinoSum >= 17 && playerSum > casinoSum && playerSum < 21){
         messageEl.textContent = "You win !"
+        gameState = GameStates.win
         endGame()
+        betResults()
         return
-        didPlayerWon = true
+    }
+
+    if(casinoSum < 17 && playerSum >=17)
+    {
+        newCard(forPlayer = false)
     }
 }
 
@@ -78,9 +99,11 @@ const surrenderButton = document.getElementById('surrender-button')
 
 function surrender() {
     messageEl.textContent = "You've lost - Casino wins!"
-    didPlayerSurrender = true
+    gameState = GameStates.surrender
     endGame()
+    betResults()
     return
+
 }
 
 function checkGameState() {
@@ -89,47 +112,59 @@ function checkGameState() {
         while(casinoSum < 17) {
             addCard(false)
         }
+        gameState = GameStates.win
         endGame()
-        return 
-        didPlayerWon = true
+        betResults()
+        return
+
     }
 
     if(casinoSum === BLACK_JACK_VALUE){
         messageEl.textContent = "You've lost - Casino wins!"
+        gameState = GameStates.lose
         endGame()
+        betResults()
         return
-        didPlayerWon = false
+
     }
 
     if( casinoSum > BLACK_JACK_VALUE && playerSum < casinoSum){
         messageEl.textContent = "You win !"
+        gameState = GameStates.win
         endGame()
+        betResults()
         return
-        didPlayerWon = true
+
     }
+
     if (playerSum > BLACK_JACK_VALUE){
         messageEl.textContent = "You've lost :("
+        gameState = GameStates.lose
         endGame()
+        betResults()
         return
-        didPlayerWon = false
 
     }
 
     if(casinoSum > 17 && playerSum > casinoSum && playerSum < 21){
         messageEl.textContent = "You win !"
+        gameState = GameStates.win
         endGame()
+        betResults()
         return
-        didPlayerWon = true
     }
+    
 }
 
 function endGame() {
-    startButton.disabled = false
+    startButton.disabled = true
     newCardButton.disabled = true
     standButton.disabled = true
     surrenderButton.disabled = true
     betButton.disabled = false
-    betResults()
+
+    scoreBoard()
+    
 }
 
 function addCard(forPlayer) {
@@ -155,7 +190,7 @@ function addCard(forPlayer) {
         casinoCardsEl.textContent += ` ${card}`//
         casinoSumEl.textContent = `Sum: ${casinoSum}`//
     }
-    
+
 }
 
 function bet(){
@@ -182,29 +217,52 @@ function bet(){
 
     betAmountOutputEl.textContent = `Current Bet : ${justFuckingBet}`
     playerCashEl.textContent = `Player Cash : ${playerCash}`
+
     
 }
 
 function betResults() {
-    if(didPlayerSurrender){
+    let afterGameMassage = document.getElementById("after-game-message")
+    let betAmount = document.getElementById("bet-amount").value
+    
+    if(gameState === GameStates.surrender){
+        playerCash -= Math.floor(betAmount*0.5)
+        afterGameMassage.textContent = `You've lost : ${Math.floor(betAmount*0.5)}`
+    } 
+    
+    if(gameState === GameStates.win){
+        playerCash += Math.floor(betAmount*1.66)
+        afterGameMassage.textContent = `You've won ${Math.floor(betAmount*1.66)}`
+    } 
+    
+    if(gameState === GameStates.lose) {
         playerCash -= betAmount
-    }
-
-    if(didPlayerWon){
-        playerCash += betAmount*1.66
-    } else {
-        playerCash -= betAmount
+        afterGameMassage.textContent = `You've lost ${betAmount}`
     }
 
     playerCashEl.textContent = `Player Cash : ${playerCash}`
+    startButton.disabled = false
+    
+
+}
+
+let scoreBoardInfo = document.getElementById("score-board")
+
+function scoreBoard(){
+    let betAmount = document.getElementById("bet-amount").value
+
+    if(gameState === GameStates.surrender){
+        document.getElementById("score-board").innerHTML += ` <p class="scoreBoardYellow">You've lost : ${Math.floor(betAmount*0.5)}</p>`
+    }
+
+    if(gameState === GameStates.win){
+        document.getElementById("score-board").innerHTML += ` <p class="scoreBoardGreen">You've won ${Math.floor(betAmount*1.66)}</p>`
+    }
+
+    if(gameState === GameStates.lose){
+        document.getElementById("score-board").innerHTML += ` <p class="scoreBoardRed">You've lost ${betAmount}</p>`
+    }
+
 }
 
 
-
-
-
-   // let betAmount = betAmountEl.value
-   // playerBetEl.textContent = `Current Bet : ${playerBetEl}`
-
-
-//function betAmountCheck() {}

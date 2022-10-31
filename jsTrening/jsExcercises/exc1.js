df116e402b2c2cc13faaa9bad967ad952e7064a4
Maxine 001 <264492@student.pwr.edu.pl>
2022-10-31 20:38:35 +0100

    exc1

--- jsTrening/jsExcercises/exc1.js
+++ jsTrening/jsExcercises/exc1.js
@@ -1,12 +1,18 @@
-let nums = [2, 7, 11, 15]
-let target = 9
-
-for(let i = 0; i < nums.length; i++){
-    let tmp = target - nums[i]
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+ var twoSum = function(nums, target) {
+    for(let i = 0; i < nums.length; i++){
+        let tmp = target - nums[i]
         for ( let j = 0; j < nums.length; j++){
-            if ( nums[j] == tmp ){
-                let tmpArray = [i , j]
-                return tmpArray
+                if ( tmp === nums[j] && i !== j){
+                    
+                    return [i , j]
+                }
             }
         }
-    }
+};
+
+console.log(twoSum([3,2,4], 6))

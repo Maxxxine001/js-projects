c00fcab0057a12805aca464becb06f47214407ef
Maxine 001 <264492@student.pwr.edu.pl>
2022-10-31 17:05:29 +0100

    uploading chrome extencion, working local storage

--- jsTrening/chromeExtension/index.js
+++ jsTrening/chromeExtension/index.js
@@ -6,17 +6,23 @@ let i = 0
 
 
 inputButton.addEventListener("click", function(){
-   inputText = document.getElementById("input-el").value
-   myLinks.push(inputText)
-   console.log(myLinks)
-   document.getElementById("link-el").innerHTML += `<p class='link'>link ${i + 1}: <a href='${myLinks[i]}' class="link" target='_blank'>${myLinks[i]}</a></p>`
-   i += 1
-   document.getElementById("input-el").value = ''
+    inputText = document.getElementById("input-el").value
+    myLinks.push(inputText)
+    console.log(myLinks)
+    document.getElementById("link-el").innerHTML += `<p class='link'><a href='${myLinks[i]}' class="link" target='_blank'>${myLinks[i]}</a></p>`
+    i += 1
+    document.getElementById("input-el").value = ''
 
     localStorage.setItem("myLinks", JSON.stringify(myLinks))
-    console.log(localStorage)
+   // console.log(localStorage.getItem("myLinks"))
 })
 
+let linksFromLocalStorage = JSON.parse(localStorage.getItem( "myLinks"))
+console.log(linksFromLocalStorage)
+
+if(linksFromLocalStorage){
+    document.getElementById("link-el").innerHTML += `<p class="link"><a target='_black' class="link" href="${linksFromLocalStorage}"> ${linksFromLocalStorage}</a></p>`
+}
 
 
 
